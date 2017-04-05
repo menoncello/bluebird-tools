283ef5b235f9bf134f627fbcc02845f9af6ace69
Eduardo Menoncello <eduardo.menoncello@gmail.com>
2017-04-05 15:23:37 -0300

    adding convert test

--- test/manipulation/convert.specs.js
+++ test/manipulation/convert.specs.js
@@ -4,12 +4,18 @@ const sinon = require('sinon');
 
 describe('manipulation - #convert', () => {
 	context('static', () => {
-		it('when passing vanilla promise, convert to a Bluebird promise', () => {
+		it('when passing a vanilla promise, convert to a Bluebird promise', () => {
 			const vanillaPromise = new Promise((res) => res());
 			const promise = BluebirdPromise.convert(vanillaPromise);
 
 			assert.isUndefined(vanillaPromise.isBluebird);
 			assert.isTrue(promise.isBluebird);
+		});
+		it('when passing a bluebird promise, returns the same promise', () => {
+			const promise = BluebirdPromise.resolve();
+			const bluebirdPromise = BluebirdPromise.convert(promise);
+
+			assert.equal(promise, bluebirdPromise);
 		});
 	});
 });
