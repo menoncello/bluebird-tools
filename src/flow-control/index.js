d01ccffcfa0b631ba461d5c5e22d9025a960882b
Eduardo Menoncello <eduardo.menoncello@gmail.com>
2017-04-05 15:25:57 -0300

    creating monitor separation

--- src/flow-control/index.js
+++ src/flow-control/index.js
@@ -2,13 +2,9 @@ module.exports = (Promise) => {
 	require('./for')(Promise);
 	require('./for-all')(Promise);
 	require('./iif')(Promise);
-	require('./iif-monitor')(Promise);
 	require('./monitor')(Promise);
-	require('./then-monitor')(Promise);
 	require('./unless')(Promise);
 	require('./unless-log')(Promise);
-	require('./unless-monitor')(Promise);
 	require('./when')(Promise);
 	require('./when-log')(Promise);
-	require('./when-monitor')(Promise);
 };
--- src/index.js
+++ src/index.js
@@ -3,5 +3,6 @@ const Promise = require('bluebird');
 require('./flow-control')(Promise);
 require('./log')(Promise);
 require('./manipulation')(Promise);
+require('./monitor')(Promise);
 
 module.exports = Promise;
